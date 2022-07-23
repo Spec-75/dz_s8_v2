17a7216d69a6fa93ffd6814126bc5574f7f00319
Spec-75 <Spec_75@mail.ru>
2022-07-23 17:52:25 +0300

    add task_58

---
+++ task_58/Program.cs
@@ -0,0 +1,66 @@
+﻿// Задача 58: Задайте две матрицы. Напишите программу, 
+// которая будет находить произведение двух матриц.
+// Например, заданы 2 массива:
+// 1 4 7 2
+// 5 9 2 3
+// 8 4 2 4
+// 5 2 6 7
+// и
+// 1 5 8 5
+// 4 9 4 2
+// 7 2 2 6
+// 2 3 4 7
+// Их произведение будет равно следующему массиву:
+// 1 20 56 10
+// 20 81 8 6
+// 56 8 4 24
+// 10 6 24 49
+
+Console.WriteLine("enter the dimension of the array:");
+int n = Convert.ToInt32(Console.ReadLine());
+int[,] firstArray = GetArray(n, n, 1, 10);
+int[,] secondArray = GetArray(n, n, 1, 10);
+
+Console.WriteLine("first array:");
+PrintArray(firstArray);
+Console.WriteLine("second array:");
+PrintArray(secondArray);
+Console.WriteLine("matrix multiplication:");
+ChangesArray(firstArray, secondArray);
+
+int[,] GetArray(int m, int n, int min, int max)
+{
+    int[,] result = new int[m, n];
+    for (int i = 0; i < m; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            result[i, j] = new Random().Next(min, max + 1);
+        }
+    }
+    return result;
+}
+
+void ChangesArray(int[,] matrix1, int[,] matrix2)
+{
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            Console.Write(matrix1[i, j] * matrix2[i, j] + "\t");
+        }
+        Console.WriteLine();
+    }
+}
+
+void PrintArray(int[,] inputArray)
+{
+    for (int i = 0; i < inputArray.GetLength(0); i++)
+    {
+        for (int j = 0; j < inputArray.GetLength(1); j++)
+        {
+            Console.Write(inputArray[i, j] + "\t");
+        }
+        Console.WriteLine();
+    }
+}
